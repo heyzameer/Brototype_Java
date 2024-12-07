1bd09b4596a6a476ecda13db64e5d0e75c3b35fa
heyzameer <khanzameer5890@gmail.com>
2024-12-07 22:32:23 +0530

    Fumigation Day 6

--- src/Arrays/fumigation/FrequencyCounter.java
+++ src/Arrays/fumigation/FrequencyCounter.java
@@ -2,7 +2,7 @@ package Arrays.fumigation;
 
 public class FrequencyCounter {
         public static void main(String[] args) {
-            int[] arr = {1, 3, 3, 3, 1};
+            int[] arr = {1, 3, 3, 3, 1,5};
             int[] freq = new int[arr.length];
 
             for (int i = 0; i < arr.length; i++) {
