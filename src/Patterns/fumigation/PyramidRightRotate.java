93a9c2efe488d16b06221abfee825689a91da317
heyzameer <khanzameer5890@gmail.com>
2024-12-08 13:10:06 +0530

    Fumigation Day 7

--- src/Patterns/fumigation/PyramidRightRotate.java
+++ src/Patterns/fumigation/PyramidRightRotate.java
@@ -6,7 +6,7 @@ public class PyramidRightRotate {
 
             // Combined loop for both upper and lower parts
             for (int i = 1; i <= 2 * n - 1; i++) {
-                int row = (i <= n) ? i : (2 * n - i); // Determine the current row dynamically
+                int row = (i <= n) ? i : (2*n -i); // Determine the current row dynamically
 
                 // Print stars
                 for (int j = 1; j <= row; j++) {
--- src/Patterns/fumigation/X_Patternn_Empty.java
+++ src/Patterns/fumigation/X_Patternn_Empty.java
@@ -1,4 +1,17 @@
 package Patterns.fumigation;
 
 public class X_Patternn_Empty {
+    public static void main(String[] args) {
+        int n = 10;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if(i==j||j==n-i-1){
+                    System.out.print(" ");
+                }else{
+                    System.out.print("*");
+                }
+            }
+            System.out.println();
+        }
+    }
 }
